fd2396e56f14b7f85f66b58aa25de88044f17504
root <root@ubuntu-14.04-amd64-vbox>
2015-12-19 11:15:12 +0000

    “jenkins-postfix-cookbook”

--- chef-repo/site-cookbooks/factorialapp/recipes/default.rb
+++ chef-repo/site-cookbooks/factorialapp/recipes/default.rb
@@ -17,3 +17,13 @@ package "python-pip"
 execute "install-python-module"do
 	command "pip install -r #{app_path}/requirements.txt"
 end
+
+if app_path != "/vagrant"
+	cookbook_file "/etc/init/factorialapi.conf" do
+		source "factorialapi.conf"
+	end
+	service "factorialapi" do
+		provider Chef::Provider::Service::Upstart
+		action [ :enable, :start ]
+	end
+end
